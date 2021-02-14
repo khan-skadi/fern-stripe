a02ea369b44088211bcf0b306298c2f5019dc0da
khan-skadi <kartalov.pt@gmail.com>
2021-02-14 23:07:33 +0100

    1

--- client/src/Payments.js
+++ client/src/Payments.js
@@ -11,7 +11,6 @@ function Payments() {
 
   // Create a payment intent on the server
   const createPaymentIntent = async (event) => {
-
     // Clamp amount to Stripe min/max
     const validAmount = Math.min(Math.max(amount, 50), 9999999);
     setAmount(validAmount);
@@ -28,11 +27,8 @@ function Payments() {
     const cardElement = elements.getElement(CardElement);
 
     // Confirm Card Payment
-    const {
-      paymentIntent: updatedPaymentIntent,
-      error,
-    } = await stripe.confirmCardPayment(paymentIntent.client_secret, {
-      payment_method: { card: cardElement },
+    const { paymentIntent: updatedPaymentIntent, error } = await stripe.confirmCardPayment(paymentIntent.client_secret, {
+      payment_method: { card: cardElement }
     });
 
     if (error) {
@@ -46,41 +42,25 @@ function Payments() {
   return (
     <>
       <h2>Payments</h2>
-      <p>
-        One-time payment scenario.
-      </p>
+      <p>One-time payment scenario.</p>
       <div className="well">
-      <PaymentIntentData data={paymentIntent} />
+        <PaymentIntentData data={paymentIntent} />
       </div>
 
-
       <div className="well">
-      <h3>Step 1: Create a Payment Intent</h3>
-      <p>
-        Change the amount of the payment in the form, then request a Payment
-        Intent to create context for one-time payment. Min 50, Max 9999999
-      </p>
+        <h3>Step 1: Create a Payment Intent</h3>
+        <p>Change the amount of the payment in the form, then request a Payment Intent to create context for one-time payment. Min 50, Max 9999999</p>
 
-      <div className="form-inline">
-        <input
-          className="form-control"
-          type="number"
-          value={amount}
-          disabled={paymentIntent}
-          onChange={(e) => setAmount(e.target.value)}
-        />
-        <button
-          className="btn btn-success"
-          disabled={amount <= 0}
-          onClick={createPaymentIntent}
-          hidden={paymentIntent}>
-          Ready to Pay ${ (amount / 100).toFixed(2) }
-        </button>
-      </div>
+        <div className="form-inline">
+          <input className="form-control" type="number" value={amount} disabled={paymentIntent} onChange={(e) => setAmount(e.target.value)} />
+          <button className="btn btn-success" disabled={amount <= 0} onClick={createPaymentIntent} hidden={paymentIntent}>
+            Ready to Pay ${(amount / 100).toFixed(2)}
+          </button>
+        </div>
       </div>
       <hr />
 
-      <form onSubmit={handleSubmit} className="well" hidden={!paymentIntent || paymentIntent.status === 'succeeded' }>
+      <form onSubmit={handleSubmit} className="well" hidden={!paymentIntent || paymentIntent.status === 'succeeded'}>
         <h3>Step 2: Submit a Payment Method</h3>
         <p>Collect credit card details, then submit the payment.</p>
         <p>
@@ -106,19 +86,16 @@ function PaymentIntentData(props) {
     const { id, amount, status, client_secret } = props.data;
     return (
       <>
-      <h3>Payment Intent <span
-          className={
-            'badge ' + ( status === 'succeeded' ? 'badge-success' : 'badge-secondary')
-          }>
-          {status}
-        </span></h3>
-      <pre>
-        ID: {id} <br />
-        Client Secret: {client_secret} <br />
-        Amount: {amount} <br />
-        Status:{status}
-        <br />
-      </pre>
+        <h3>
+          Payment Intent <span className={'badge ' + (status === 'succeeded' ? 'badge-success' : 'badge-secondary')}>{status}</span>
+        </h3>
+        <pre>
+          ID: {id} <br />
+          Client Secret: {client_secret} <br />
+          Amount: {amount} <br />
+          Status:{status}
+          <br />
+        </pre>
       </>
     );
   } else {
